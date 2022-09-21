26903f1287ffcb51eceb18562ae4463877e9e5b2
Nguyen Van Tai <tai.nguyenvan@becamex.com>
2022-09-21 22:47:01 +0700

    add UserService

---
+++ IoTPlatform.Infrastructure/Services/Interfaces/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IoTPlatform.Infrastructure.Services.Interfaces
+{
+    public interface IUserService
+    {
+        string[] GetUserInformation();
+    }
+}
---
+++ IoTPlatform.Infrastructure/Services/UserService.cs
@@ -0,0 +1,38 @@
+﻿using IoTPlatform.Infrastructure.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IoTPlatform.Infrastructure.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IHttpContextAccessor _httpContextAccesstor;
+
+        public UserService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccesstor = httpContextAccessor;
+        }
+
+        public string[] GetUserInformation()
+        {
+            var userInfor = new string[2];
+            
+            if (_httpContextAccesstor.HttpContext != null)
+            {
+                var userName = _httpContextAccesstor.HttpContext.User.FindFirst(claim => claim.Type == "UserName");
+                var userId = _httpContextAccesstor.HttpContext.User.FindFirst(claim => claim.Type == "UserId");
+
+                if (userName != null && userId != null)
+                {
+                    userInfor[0] = userName.Value;
+                    userInfor[1] = userId.Value;
+                }
+            }
+            return userInfor;
+        }
+    }
+}
