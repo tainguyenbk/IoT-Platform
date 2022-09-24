eb1c88397d688736f290da8046b1f4cc721f7396
Nguyen Van Tai <tai.nguyenvan@becamex.com>
2022-09-24 15:46:56 +0700

    commit

--- IoTPlatform.Domain/Models/Device/DeviceProfile.cs
+++ IoTPlatform.Domain/Models/Device/DeviceProfile.cs
@@ -6,7 +6,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace IoTPlatform.Domain.Models.Device
 {
