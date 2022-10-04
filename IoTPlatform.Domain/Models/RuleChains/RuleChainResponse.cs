99bbc88ca546a4244ab9c26b42b790cdcda750f7
Nguyen Van Tai <tai.nguyenvan@becamex.com>
2022-10-04 22:13:59 +0700

    add RuleChainResponse

---
+++ IoTPlatform.Domain/Models/RuleChains/RuleChainResponse.cs
@@ -0,0 +1,15 @@
+﻿using IoTPlatform.Domain.Models.AuditLogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IoTPlatform.Domain.Models.RuleChains
+{
+    public class RuleChainResponse
+    {
+        public RuleChain RuleChain { get; set; } 
+        public List<AuditLog> AuditLogs { get; set; }
+    }
+}
